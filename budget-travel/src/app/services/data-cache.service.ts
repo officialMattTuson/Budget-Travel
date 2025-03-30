98a546315a1fb6092e630760ac454f14856900da
Matthew Tuson <mtuson1414@gmail.com>
2025-03-30 19:47:43 +1300

    refactor currency logic from data cache into currency service

--- budget-travel/src/app/services/data-cache.service.ts
+++ budget-travel/src/app/services/data-cache.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
 import { Budget } from '../models/budgets.model';
 import { Expense } from '../models/expense.model';
 import { Event } from '../models/event.model';
-import { Currency } from '../models/currency.model';
-
 @Injectable({
   providedIn: 'root',
 })
@@ -18,15 +15,6 @@ export class DataCacheService {
 
   private readonly _events = new BehaviorSubject<Event[]>([]);
   public events$ = this._events.asObservable();
-
-  private readonly _currencies = new BehaviorSubject<Currency[]>([]);
-  public currencies$ = this._currencies.asObservable();
-
-  private readonly _defaultCurrency = new BehaviorSubject<Currency>({
-    code: 'NZD',
-    symbol: 'NZ$',
-  });
-  public defaultCurrency$ = this._defaultCurrency.asObservable();
 
   getBudgets(): Budget[] {
     return this._budgets.getValue();
@@ -42,53 +30,5 @@ export class DataCacheService {
 
   setExpenses(expenses: Expense[]) {
     this._expenses.next(expenses);
-  }
-
-  setCurrencies(currencies: Currency[]) {
-    this._currencies.next(currencies);
-  }
-
-  getCurrencies(): Observable<Currency[]> {
-    return this.currencies$.pipe(
-      switchMap((currencies) => {
-        if (currencies.length === 0) {
-          const allCurrencies = this.getAllCurrencies();
-          this._currencies.next(allCurrencies);
-          return of(allCurrencies);
-        }
-        return of(currencies);
-      })
-    );
-  }
-
-  getAllCurrencies(): Currency[] {
-    const currencyCodes = Intl.supportedValuesOf('currency');
-
-    return currencyCodes.map((code) => ({
-      code,
-      symbol: this.getCurrencySymbol(code, 'en'),
-    }));
-  }
-
-  getCurrencySymbol(currencyCode: string, locale: string): string {
-    try {
-      const parts = new Intl.NumberFormat(locale, {
-        style: 'currency',
-        currency: currencyCode,
-      }).formatToParts();
-      return (
-        parts.find((part) => part.type === 'currency')?.value ?? currencyCode
-      );
-    } catch (error) {
-      return currencyCode;
-    }
-  }
-
-  setDefaultCurrency(currency: Currency) {
-    this._defaultCurrency.next(currency);
-  }
-
-  getDefaultCurrency(): Currency {
-    return this._defaultCurrency.getValue();
   }
 }
--- budget-travel/src/app/services/shared/currency.service.ts
+++ budget-travel/src/app/services/shared/currency.service.ts
@@ -1,9 +1,65 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, switchMap, of } from 'rxjs';
+import { Currency } from '../../models/currency.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CurrencyService {
+  private readonly _currencies = new BehaviorSubject<Currency[]>([]);
+  public currencies$ = this._currencies.asObservable();
 
-  constructor() { }
+  private readonly _defaultCurrency = new BehaviorSubject<Currency>({
+    code: 'NZD',
+    symbol: 'NZ$',
+  });
+  public defaultCurrency$ = this._defaultCurrency.asObservable();
+
+  setCurrencies(currencies: Currency[]) {
+    this._currencies.next(currencies);
+  }
+
+  getCurrencies(): Observable<Currency[]> {
+    return this.currencies$.pipe(
+      switchMap((currencies) => {
+        if (currencies.length === 0) {
+          const allCurrencies = this.getSupportedCurrencies();
+          this._currencies.next(allCurrencies);
+          return of(allCurrencies);
+        }
+        return of(currencies);
+      })
+    );
+  }
+
+  getSupportedCurrencies(): Currency[] {
+    const currencyCodes = Intl.supportedValuesOf('currency');
+
+    return currencyCodes.map((code) => ({
+      code,
+      symbol: this.getCurrencySymbol(code, 'en'),
+    }));
+  }
+
+  getCurrencySymbol(currencyCode: string, locale: string): string {
+    try {
+      const parts = new Intl.NumberFormat(locale, {
+        style: 'currency',
+        currency: currencyCode,
+      }).formatToParts();
+      return (
+        parts.find((part) => part.type === 'currency')?.value ?? currencyCode
+      );
+    } catch (error) {
+      return currencyCode;
+    }
+  }
+
+  setDefaultCurrency(currency: Currency) {
+    this._defaultCurrency.next(currency);
+  }
+
+  getDefaultCurrency(): Currency {
+    return this._defaultCurrency.getValue();
+  }
 }
