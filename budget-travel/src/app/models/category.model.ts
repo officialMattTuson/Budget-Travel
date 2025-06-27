d289a8ce68a5fcfa9dd044a1b4358006cfc8d1d4
Matthew Tuson <mtuson1414@gmail.com>
2025-06-28 10:48:36 +1200

    add color property to Category model and implement color assignment in CategoriesService

--- budget-travel/src/app/models/category.model.ts
+++ budget-travel/src/app/models/category.model.ts
@@ -1,9 +1,17 @@
 export interface Category {
   id: number;
   name: string;
+  color?: string;
 }
 
 export interface CategoryBudget {
   category: string;
   allocatedAmount: number;
+}
+
+export class CategoryColor {
+  public static readonly palette = ['#42a2da', '#fbbc05', '#34a853', '#ea4335', '#ab47bc'];
+  static getColorById(id: number): string {
+    return CategoryColor.palette[id % CategoryColor.palette.length];
+  }
 }
--- budget-travel/src/app/services/shared/categories.service.ts
+++ budget-travel/src/app/services/shared/categories.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { Category } from '../../models/category.model';
+import { BehaviorSubject, Observable, map } from 'rxjs';
+import { Category, CategoryColor } from '../../models/category.model';
 
 @Injectable({
   providedIn: 'root',
@@ -17,7 +17,14 @@ export class CategoriesService {
   public categories$ = this._categories.asObservable();
 
   getCategories(): Observable<Category[]> {
-    return this.http.get<Category[]>(`${this.apiUrl}`);
+    return this.http.get<Category[]>(`${this.apiUrl}`).pipe(
+      map((categories) =>
+        categories.map((cat) => ({
+          ...cat,
+          color: CategoryColor.getColorById(cat.id),
+        }))
+      )
+    );
   }
 
   // Temporarily storing these methods here until facade is implemented
