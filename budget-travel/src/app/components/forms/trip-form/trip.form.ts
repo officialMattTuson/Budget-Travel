2e30813247363b466b3f78ca5b50710774205e66
Matthew Tuson <mtuson1414@gmail.com>
2025-06-28 10:49:17 +1200

    add TripForm component with form validation and category breakdown management

---
+++ budget-travel/src/app/components/forms/trip-form/trip.form.ts
@@ -0,0 +1,57 @@
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
+import {
+  decimalRegex,
+  maxNameLength,
+  minDescriptionLength,
+} from '../../../utils/validators/validation-consts';
+import { Category } from '../../../models/category.model';
+
+const getTripForm = () => {
+  const fb = new FormBuilder();
+  return fb.group({
+    name: fb.control('', [
+      Validators.required,
+      Validators.minLength(minDescriptionLength),
+      Validators.maxLength(maxNameLength),
+    ]),
+    destination: fb.control('', [
+      Validators.required,
+      Validators.minLength(minDescriptionLength),
+      Validators.maxLength(maxNameLength),
+    ]),
+    startDate: fb.control('', [Validators.required]),
+    endDate: fb.control('', [Validators.required]),
+    currency: fb.control('', Validators.required),
+    totalBudget: fb.control('', [
+      Validators.required,
+      Validators.min(0),
+      Validators.pattern(decimalRegex),
+    ]),
+    categoryBreakdown: fb.array([]),
+  });
+};
+
+export class TripForm extends FormGroup {
+  constructor() {
+    super(getTripForm().controls);
+  }
+
+  get categoryBreakdown(): FormArray {
+    return this.get('categoryBreakdown') as FormArray;
+  }
+
+  addCategoryBreakdown(categories: Category[]): void {
+    this.categoryBreakdown.clear();
+    categories.forEach((cat) => {
+      this.categoryBreakdown.push(
+        new FormGroup({
+          categoryId: new FormBuilder().control(cat.id),
+          amount: new FormBuilder().control(0, [
+            Validators.required,
+            Validators.min(0),
+          ]),
+        })
+      );
+    });
+  }
+}
